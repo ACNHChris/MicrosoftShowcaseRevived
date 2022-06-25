a4b1212de0e76c9c8f8949c066a70d3e609e7d3f
Chris <105169246+ACNHChris@users.noreply.github.com>
2022-06-25 12:46:18 +0100

    Batch update

---
+++ js/script.js
@@ -0,0 +1,4 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-welcome-to-microsoft-showcase","displayName":"Welcome to Microsoft Showcase","link":{"linkType":"LinkTypePage","href":"#!page-welcome-to-microsoft-showcase"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
